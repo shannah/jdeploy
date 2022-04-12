099655c93d11dade00ae44a3c83897627fe3ceab
Steve Hannah <steve@weblite.ca>
2022-04-12 12:13:48 -0700

    Added some comments to the theme apis

--- installer/themes/fx-theme/src/main/java/ca/weblite/jdeploy/themes/fxtheme/FXApplication.java
+++ installer/themes/fx-theme/src/main/java/ca/weblite/jdeploy/themes/fxtheme/FXApplication.java
@@ -9,21 +9,40 @@ import javafx.scene.image.Image;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
+/**
+ * The FXApplication class used to launch JavaFX.
+ */
 public class FXApplication extends Application {
+
     static FXInstallationForm installationForm;
     private Button install;
     @Override
     public void start(Stage stage) throws Exception {
+        // Set a back-link to the application instance so that
+        // the installationForm can interact with the it.
         installationForm.app = this;
+
+        // We set this flag to inform the UI that JavaFX is initialized.
+        // The Platform.runLater() method will throw an exception if used
+        // before JavaFX is initialized.  This flag is used to ensure that
+        // we don't use JavaFX until it is ready.
         FXUI.initialized = true;
+
+        // Create an "Install" button that the user presses to proceed with installation.
         install = new Button("Install");
+
+        // Trigger an InstallClicked event when install is pressed.  This will
+        // tell the Installer to do its thing.
         install.setOnAction(evt->installationForm.getEventDispatcher().fireEvent(InstallationFormEvent.Type.InstallClicked));
+
         var scene = new Scene(new StackPane(install), 640, 480);
         stage.setScene(scene);
         stage.show();
     }
 
     void setInProgress(boolean inProgress, String message) {
+        // While installation is in progress, we disable the install button
+        // so that it can't be triggered twice.
         if (inProgress) {
             install.setDisable(true);
         } else {
--- installer/themes/fx-theme/src/main/java/ca/weblite/jdeploy/themes/fxtheme/FXInstallationForm.java
+++ installer/themes/fx-theme/src/main/java/ca/weblite/jdeploy/themes/fxtheme/FXInstallationForm.java
@@ -6,15 +6,41 @@ import ca.weblite.jdeploy.installer.models.InstallationSettings;
 import ca.weblite.jdeploy.installer.views.InstallationForm;
 import javafx.scene.control.Alert;
 
+/**
+ * The InstallationForm instance that wraps the JavaFX installation form. The {@link #showInstallationForm()}}
+ * method is what actually launches JavaFX and displays the form, but it also includes other methods
+ * that the Installer needs to use to interact with the UI.
+ */
 public class FXInstallationForm implements InstallationForm {
+
+    // The event dispatcher used for notifying the installer of important
+    // events, like when the user presses "Install"
     private InstallationFormEventDispatcher dispatcher;
+
+    // The installation settings.  You should be able to get all of the information
+    // you need about the app being installed, and the install environment from this
+    // object.
     private InstallationSettings installationSettings;
+
+    // Reference to the FXApplication app.
     FXApplication app;
+
+    /**
+     * Creates a new InstallationForm
+     * @param settings The pre-populated installation settings.
+     */
     public FXInstallationForm(InstallationSettings settings) {
         this.installationSettings = settings;
         FXApplication.installationForm = this;
     }
 
+    /**
+     * Displays the dialog when installation is successful and complete.  Should
+     * give the user the option to open the app, reveal the app in the explorer/finder,
+     * or simply close the installer.
+     *
+     *
+     */
     @Override
     public void showInstallationCompleteDialog() {
         Alert alert = new Alert(Alert.AlertType.INFORMATION);
@@ -22,27 +48,54 @@ public class FXInstallationForm implements InstallationForm {
         alert.setHeaderText("Installation Complete");
         alert.setContentText("Installation is complete");
         alert.showAndWait();
+        // In this base-bones implementation we simply close the installer on complete.
         getEventDispatcher().fireEvent(InstallationFormEvent.Type.InstallCompleteCloseInstaller);
+
+        // Full implementations should give the user the option to open the app
+        // or reveal the app in the explorer
+        // getEventDispatcher().fireEvent(InstallationFormEvent.Type.InstallCompleteOpenApp);
+        // getEventDispatcher().fireEvent(InstallationFormEvent.Type.InstallCompleteRevealApp);
     }
 
+    /**
+     * Shows the installation form.  In this case, it initializes JavaFX also.
+     */
     @Override
     public void showInstallationForm() {
-
+        // Show the installation form.
         FXApplication.launch(FXApplication.class);
     }
 
+    /**
+     * Sets the event dispatcher.  The Installer will use this to set the event dispatcher in this
+     * installer as soon as it is instantiated.
+     * @param dispatcher The dispatcher to use to send event notifications to the Installer.
+     * @see InstallationFormEvent.Type For types of events to send.
+     */
     @Override
     public void setEventDispatcher(InstallationFormEventDispatcher dispatcher) {
         this.dispatcher = dispatcher;
     }
 
+    /**
+     * Gets the event dispatcher.
+     * @return
+     */
     @Override
     public InstallationFormEventDispatcher getEventDispatcher() {
         return dispatcher;
     }
 
+    /**
+     * Sets and unsets the in-progress status.
+     * @param inProgress True if installation is in progress and the UI should show some sort of progress indicator.
+     * @param message Text that can be displayed describing the progress.
+     */
     @Override
     public void setInProgress(boolean inProgress, String message) {
+        // Sets whether installation is in progress
+        // Usually this is only a couple of seconds, but you can show an indicator
+        // while in progress is true.
         app.setInProgress(inProgress, message);
     }
 }
