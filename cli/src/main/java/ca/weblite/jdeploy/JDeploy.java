bef1c5ff3bbfd8f0483e18717e8553d88dcfad86
Steve Hannah <steve@weblite.ca>
2022-12-11 15:31:46 -0800

    Fixup

--- cli/src/main/java/ca/weblite/jdeploy/JDeploy.java
+++ cli/src/main/java/ca/weblite/jdeploy/JDeploy.java
@@ -1862,13 +1862,13 @@ public class JDeploy {
         if ("branch".equals(refType)) {
             notes.append("\nOr launch app installer via command-line on Linux, Mac, or Windows:\n\n");
             notes.append("```bash\n");
-            notes.append("/bin/bash -c \"$(curl -fsSL https://www.jdeploy.com/")
+            notes.append("/bin/bash -c \"$(curl -fsSL https://www.jdeploy.com/gh/")
                     .append(repo).append("/").append(branchTag).append("/install.sh)\"\n");
             notes.append("```\n");
         } else {
             notes.append("\nOr launch app installer via command-line on Linux, Mac, or Windows:\n\n");
             notes.append("```bash\n");
-            notes.append("/bin/bash -c \"$(curl -fsSL https://www.jdeploy.com/").append(repo).append("/install.sh)\"\n");
+            notes.append("/bin/bash -c \"$(curl -fsSL https://www.jdeploy.com/gh/").append(repo).append("/install.sh)\"\n");
             notes.append("```\n");
         }
         notes.append("\nSee [download page](https://www.jdeploy.com/gh/").append(repo).append("/").append(branchTag).append(") for more download options.\n\n");
