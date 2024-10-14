405784cc54930acb68fbef431c9daeb9e5a5b6ee
Steve Hannah <steve@weblite.ca>
2024-10-14 08:01:19 -0700

    skip test that hangs on github actions

--- shared/src/test/java/ca/weblite/tools/security/MacKeyStoreKeyProviderTest.java
+++ shared/src/test/java/ca/weblite/tools/security/MacKeyStoreKeyProviderTest.java
@@ -6,10 +6,12 @@ import static org.junit.jupiter.api.condition.OS.MAC;
 import java.security.*;
 import java.security.cert.Certificate;
 import org.junit.jupiter.api.*;
+import org.junit.jupiter.api.condition.DisabledIfEnvironmentVariable;
 import org.junit.jupiter.api.condition.EnabledIfEnvironmentVariable;
 import org.junit.jupiter.api.condition.EnabledOnOs;
 
 @EnabledIfEnvironmentVariable(named = "JDEPLOY_TEST_CERTIFICATE_NAME", matches = ".+")
+@DisabledIfEnvironmentVariable(named = "GITHUB_ACTIONS", matches = "true")
 @EnabledOnOs(MAC)
 public class MacKeyStoreKeyProviderTest {
     private String alias;
