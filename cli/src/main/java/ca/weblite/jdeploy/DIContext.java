48e992a952d8cff7d99afff422d61ddb2ca0508a
Steve Hannah <steve@weblite.ca>
2025-01-01 14:04:55 -0800

    feat: pluggable dependency injection

--- cli/src/main/java/ca/weblite/jdeploy/DIContext.java
+++ cli/src/main/java/ca/weblite/jdeploy/DIContext.java
@@ -5,14 +5,31 @@ import ca.weblite.jdeploy.di.JDeployModule;
 import ca.weblite.jdeploy.openai.di.OpenAiModule;
 import org.codejargon.feather.Feather;
 
+import java.util.Arrays;
+
 public class DIContext {
 
-    private final Feather feather = Feather.with(new JDeployModule(), new OpenAiModule(), new JDeployCliModule());
+    private final Feather feather;
 
     private static DIContext instance;
 
+    public DIContext(Object ...modules) {
+        java.util.List<Object> args = new java.util.ArrayList<Object>();
+        args.add(new JDeployModule());
+        args.add(new OpenAiModule());
+        args.add(new JDeployCliModule());
+        args.addAll(Arrays.asList(modules));
+        feather = Feather.with(args);
+    }
+
     public <T> T getInstance(Class<T> clazz) {
         return feather.instance(clazz);
+    }
+
+    public static void initialize(Object ...modules) {
+        synchronized (DIContext.class) {
+            instance = new DIContext(modules);
+        }
     }
 
     public static DIContext getInstance() {
@@ -26,5 +43,9 @@ public class DIContext {
 
         return instance;
 
+    }
+
+    public static <T> T get(Class<T> clazz) {
+        return getInstance().getInstance(clazz);
     }
 }
