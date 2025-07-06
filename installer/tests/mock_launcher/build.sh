a189cc3a25559c86640f2a09a228d2d86b1f1c6d
Steve Hannah <steve@weblite.ca>
2025-07-06 11:40:53 -0600

    revert mock launcher changes

--- installer/tests/mock_launcher/build.sh
+++ installer/tests/mock_launcher/build.sh
@@ -1,11 +1,3 @@
 #!/bin/bash
 set -e
-
-# Check if the system is Windows ARM, and force AMD64 compilation
-if [[ $(uname -m) == "aarch64" ]]; then
-    # Force compilation for amd64 on Windows ARM
-    gcc -o mock_launcher_win.exe mock_launcher_win.c --std=c99 -march=x86-64
-else
-    # Normal compilation for other architectures
-    gcc -o mock_launcher_win.exe mock_launcher_win.c --std=c99
-fi
+gcc -o mock_launcher_win.exe mock_launcher_win.c --std=c99
